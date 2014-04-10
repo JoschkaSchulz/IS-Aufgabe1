5a322f7178c265e6b7c9a36b6e41f2445997898c
J. Schulz <Horstyschulz@gmail.com>
2014-04-10 09:09:37 +0200

    added new tests

--- praktikum_test.pl
+++ praktikum_test.pl
@@ -2,15 +2,35 @@
 :-consult(stammbaumkopie).
 
 test(a):- true.
+
+test(vater):- vater(harry, charles),!.
+test(vater_fail1, [fail]):- vater(charles, harry),!.
+test(vater_fail2, [fail]):- vater(harry,diana),!.
+
+test(mutter):- mutter(harry,diana),!.
+test(mutter_fail1, [fail]):- mutter(diana,harry),!.
+test(mutter_fail2, [fail]):- mutter(harry, charles),!.
+
 test(onkel):- istOnkel(harry,george),!.
+test(onkel_fail,[fail]):- istOnkel(harry,william),!.
+
 test(opa):- opa(charles,george),!.
+test(opa_fail,[fail]):- opa(harry,diana),!.
+
 test(vorfahre):-vorfahre(georgeVI,george),!.
+test(vorfahre_fail,[fail]):- vorfahre(gorge,georgeVI),!.
+
 test(geschwister):-geschwister(william,harry),!.
-test(not_geschwister):-not(geschwister(william,kate)),!.
+test(geschwister_fail,[fail]):-geschwister(william,kate),!.
+
 test(bruder):- bruder(charles,anne),!.
-test(not_bruder):-not(bruder(anne,charles)),!.
+test(bruder_fail,[fail]):- bruder(anne,charles),!.
+
 test(schwester):-schwester(anne,charles),!.
-test(not_Tante):-not(istTante(kate,george)),!.
+test(schwester_fail,[fail]) :- schwester(charles, anne),!.
+
+test(tante_fail,[fail]):-istTante(kate,george),!.
+
 test(schwager):-schwager(timothy,camilla),!.
 
 
